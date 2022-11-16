752cad8c1c4fcc28d8e67d1320335be8bbf5f8c5
davidcoderistov <davidcoderistov@gmail.com>
2022-11-16 14:36:18 +0100

    Include authorization header when making requests

--- client/src/config/apollo.ts
+++ client/src/config/apollo.ts
@@ -1,10 +1,27 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, ApolloLink, InMemoryCache, createHttpLink } from '@apollo/client'
+import { getStorageLoggedInUser } from '../localStorage'
 
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:5000/api',
+    credentials: 'include',
+})
+
+const authLink = new ApolloLink((operation, forward) => {
+    const user = getStorageLoggedInUser()
+
+    operation.setContext({
+        headers: {
+            authorization: user?.accessToken ? `Bearer ${user.accessToken}` : "",
+        }
+    })
+
+    return forward(operation)
+})
 
 const client = new ApolloClient({
-    uri: 'http://localhost:5000/api',
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
-    credentials: 'include',
 })
 
 export default client
