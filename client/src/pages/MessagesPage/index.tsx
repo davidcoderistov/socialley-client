2cf2098c0596861819cb49a6775b5ed9eca0122e
davidcoderistov <davidcoderistov@gmail.com>
2022-11-19 15:33:58 +0100

    Create empty MessagesPage and ProfilePage components

---
+++ client/src/pages/MessagesPage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+
+
+export default function MessagesPage () {
+
+    return (
+        <Box color='#FFFFFF'>
+            Messages Page
+        </Box>
+    )
+}
---
+++ client/src/pages/ProfilePage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+
+
+export default function ProfilePage () {
+
+    return (
+        <Box color='#FFFFFF'>
+            Profile Page
+        </Box>
+    )
+}
