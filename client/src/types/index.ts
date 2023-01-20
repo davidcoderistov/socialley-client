8d19bb6587a8a412a39e196c7d11f31f229731bf
davidcoderistov <davidcoderistov@gmail.com>
2023-01-20 15:12:16 +0100

    Create multiple types on the frontend

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -5,13 +5,39 @@ export interface User {
     lastName: string
     username: string
     email: string
+    avatarURL: string | null
     accessToken: string
+}
+
+export interface PublicUser {
+    _id: string
+    firstName: string
+    lastName: string
+    username: string
+    email: string
+    avatarURL: string | null
 }
 
 export interface Message {
     _id: string
     fromUserId: string
     toUserId: string
+    message: string | null
+    photoURL: string | null
+    createdAt: number
+}
+
+interface MessageUser {
+    _id: string
+    firstName: string
+    lastName: string
+    avatarURL: string | null
+}
+
+export interface FullMessage {
+    _id: string
+    fromUser: MessageUser
+    toUser: MessageUser
     message: string | null
     photoURL: string | null
     createdAt: number
