02396f3b39f3b2b0dff3dd7d7c873e2870c5cbac
davidcoderistov <davidcoderistov@gmail.com>
2023-01-24 21:46:12 +0100

    Create useReceiveMessage hook

---
+++ client/src/hooks/graphql/messages/useReceiveMessage.ts
@@ -0,0 +1,36 @@
+import { useContext } from 'react'
+import { useSubscription } from '@apollo/client'
+import { MESSAGE_CREATED_SUBSCRIPTION } from '../../../graphql/subscriptions/messages'
+import { FullMessage } from '../../../types'
+import AppContext from '../../../config/context'
+import { User } from '../../../types'
+import { useAddLatestMessage } from './useAddLatestMessage'
+import { useAddChatMessage } from './useAddChatMessage'
+
+
+export function useReceiveMessage () {
+
+    const context = useContext(AppContext)
+    const loggedInUser = context.loggedInUser as User
+
+    const [addLatestMessage] = useAddLatestMessage()
+    const [addChatMessage] = useAddChatMessage()
+
+    useSubscription<{ messageCreated: FullMessage }>(MESSAGE_CREATED_SUBSCRIPTION, {
+        onData (data) {
+            if (!data.data.error) {
+                const message = data.data.data?.messageCreated
+                if (message) {
+                    const userId = message.fromUser._id === loggedInUser._id ?
+                        message.toUser._id : message.fromUser._id
+                    addLatestMessage(message)
+                    addChatMessage(userId, {
+                        ...message,
+                        fromUserId: message.fromUser._id,
+                        toUserId: message.toUser._id,
+                    })
+                }
+            }
+        }
+    })
+}
