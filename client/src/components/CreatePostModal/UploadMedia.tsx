3c02ea17d7d94d51ad52847c570036c0cf391583
davidcoderistov <davidcoderistov@gmail.com>
2023-02-08 16:48:33 +0100

    Create UploadMedia component

---
+++ client/src/components/CreatePostModal/UploadMedia.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import { SaveAlt } from '@mui/icons-material'
+import UploadFile from '../UploadFile'
+
+
+interface Props {
+    onChangeFile: (file: File, isVideo: boolean) => void
+}
+
+export default function UploadMedia (props: Props) {
+
+    return (
+        <Box
+            component='div'
+            display='flex'
+            flexDirection='column'
+            justifyContent='center'
+            alignItems='center'
+            height='100%'
+            width='100%'
+        >
+            <SaveAlt sx={{ fontSize: 60, color: '#FFFFFF' }}/>
+            <Typography
+                variant='h6'
+                color='#FFFFFF'
+                marginBottom='20px'
+            >Upload photos and videos here</Typography>
+            <Button
+                variant='contained'
+                color='primary'
+                component='label'
+                sx={{ textTransform: 'none' }}
+            >
+                Select from computer
+                <UploadFile mimeTypes={['image/jpeg', 'image/png', 'video/mp4']} onChangeFile={props.onChangeFile} />
+            </Button>
+        </Box>
+    )
+}
