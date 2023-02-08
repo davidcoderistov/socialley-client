812e841f218da02673776bab3e4f3d7782e98b46
davidcoderistov <davidcoderistov@gmail.com>
2023-02-08 16:52:34 +0100

    Create initial version of CreatePostModal component

---
+++ client/src/components/CreatePostModal/PostPreview.tsx
@@ -0,0 +1,198 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import InputBase from '@mui/material/InputBase'
+import UserAvatar from '../UserAvatar'
+
+
+export default function PostPreview (props: { url: string }) {
+
+    return (
+        <Box
+            component='div'
+            display='flex'
+            flex='1 1 auto'
+            margin='15px'
+        >
+            <Box
+                component='div'
+                display='flex'
+                justifyContent='center'
+                height='100%'
+                alignItems='flex-start'
+                width='450px'
+            >
+                <Box
+                    component='div'
+                    alignItems='center'
+                    border='0'
+                    boxSizing='border-box'
+                    display='flex'
+                    flexDirection='column'
+                    flexShrink='0'
+                    height='100%'
+                    justifyContent='center'
+                    margin='0'
+                    padding='0'
+                    position='relative'
+                    width='100%'
+                    sx={{ verticalAlign: 'baseline' }}
+                >
+                    <Box
+                        component='div'
+                        alignItems='center'
+                        bgcolor='#121212'
+                        border='0'
+                        boxSizing='border-box'
+                        display='flex'
+                        flexDirection='column'
+                        flexShrink='0'
+                        height='100%'
+                        justifyContent='center'
+                        margin='0'
+                        padding='0'
+                        position='relative'
+                        width='100%'
+                        sx={{ verticalAlign: 'baseline' }}
+                    >
+                        <Box
+                            component='div'
+                            alignItems='center'
+                            display='flex'
+                            flexDirection='column'
+                            height='100%'
+                            width='100%'
+                        >
+                            <Box
+                                component='div'
+                                width='100%'
+                                height='100%'
+                                alignItems='stretch'
+                                border='0'
+                                boxSizing='border-box'
+                                display='flex'
+                                flexDirection='column'
+                                flexShrink='0'
+                                margin='auto'
+                                padding='0'
+                                position='relative'
+                                sx={{ verticalAlign: 'baseline' }}
+                            >
+                                <Box
+                                    component='img'
+                                    src={props.url}
+                                    width='100%'
+                                    height='100%'
+                                    border='0'
+                                    margin='0'
+                                    padding='0'
+                                    sx={{ verticalAlign: 'baseline' }}
+                                />
+                            </Box>
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
+            <Box
+                component='div'
+                display='flex'
+                position='relative'
+                boxSizing='border-box'
+                width='340px'
+                borderLeft='1px solid #363636'
+            >
+                <Box
+                    component='div'
+                    flex='1 1 auto'
+                    minHeight='0'
+                    minWidth='0'
+                    justifyContent='flex-start'
+                    flexDirection='column'
+                    alignItems='stretch'
+                    alignContent='stretch'
+                    display='flex'
+                    boxSizing='border-box'
+                    position='relative'
+                >
+                    <Box
+                        component='div'
+                        display='block'
+                        height='100%'
+                        overflow='hidden'
+                    >
+                        <Box
+                            component='div'
+                            alignItems='stretch'
+                            border='0'
+                            boxSizing='border-box'
+                            display='flex'
+                            flexDirection='column'
+                            margin='0'
+                            padding='0'
+                            position='relative'
+                            height='100%'
+                            sx={{ verticalAlign: 'baseline' }}
+                        >
+                            <Box
+                                component='div'
+                                display='flex'
+                                margin='0 16px'
+                                flex='0 0 auto'
+                                justifyContent='flex-start'
+                                flexDirection='row'
+                                alignItems='center'
+                                alignContent='stretch'
+                                boxSizing='border-box'
+                                position='relative'
+                                columnGap='5px'
+                            >
+                                <UserAvatar
+                                    size={40}
+                                    fontSize={16}
+                                    firstName='David'
+                                    lastName='Ristov' />
+                                <Typography color='#FFFFFF'>davidristovv</Typography>
+                            </Box>
+                            <Box
+                                component='div'
+                                margin='16px'
+                                flex='1 1 auto'
+                                minHeight='0'
+                                minWidth='0'
+                                justifyContent='flex-start'
+                                flexDirection='column'
+                                alignItems='stretch'
+                                alignContent='stretch'
+                                display='flex'
+                                boxSizing='border-box'
+                                position='relative'
+                                height='100%'
+                            >
+                                <InputBase
+                                    sx={{
+                                        '& input::-webkit-outer-spin-button, & input::-webkit-inner-spin-button': {
+                                            display: 'none',
+                                        },
+                                        '& input': {
+                                            MozAppearance: 'textfield',
+                                            '&::placeholder': {
+                                                fontSize: '14px',
+                                                color: '#EEEEFF'
+                                            },
+                                        },
+                                        color: '#FFFFFF',
+                                    }}
+                                    placeholder='Write a caption...'
+                                    multiline
+                                    maxRows={10}
+                                    autoFocus
+                                    fullWidth
+                                />
+                            </Box>
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
---
+++ client/src/components/CreatePostModal/index.tsx
@@ -0,0 +1,99 @@
+import React, { useState, useMemo } from 'react'
+import Dialog from '@mui/material/Dialog'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import { Close, ArrowBack } from '@mui/icons-material'
+import UploadMedia from './UploadMedia'
+import PostPreview from './PostPreview'
+
+
+export default function CreatePostModal () {
+
+    const [step, setStep] = useState<number>(0)
+    const [file, setFile] = useState<File | null>(null)
+    const [isVideo, setIsVideo] = useState<boolean>(false)
+
+    const handleChangeFile = (file: File, isVideo: boolean) => {
+        setStep(step + 1)
+        setFile(file)
+        setIsVideo(isVideo)
+    }
+
+    const width: number = useMemo(() => {
+        if (step < 1) {
+            return 500
+        }
+        return 700
+    }, [step])
+
+    const fileUrl = useMemo(() => {
+        if (file) {
+            return URL.createObjectURL(file)
+        }
+        return null
+    }, [file])
+
+    return (
+        <Dialog
+            open={true}
+            fullWidth
+            PaperProps={{
+                sx: {
+                    backgroundColor: '#262626',
+                    borderRadius: '20px',
+                    maxWidth: width,
+                    paddingBottom: '10px',
+                    height: 500
+                }
+            }}
+        >
+            <Box
+                component='div'
+                minHeight='0'
+                minWidth='0'
+                height='100%'
+                width='100%'
+                overflow='hidden'
+                display='flex'
+                flexDirection='column'
+                justifyContent='flex-start'
+                alignItems='stretch'
+                alignContent='stretch'
+                boxSizing='border-box'
+                position='relative'
+            >
+                <Box
+                    component='div'
+                    display='flex'
+                    flexDirection='row'
+                    justifyContent='space-between'
+                    alignItems='center'
+                >
+                    { step > 0 ? (
+                        <IconButton sx={{ color: '#FFFFFF' }} onClick={() => setStep(step - 1)}>
+                            <ArrowBack />
+                        </IconButton>
+                    ) : (
+                        <Box component='div' />
+                    )}
+                    <Typography color='#FFFFFF'>Create new post</Typography>
+                    <IconButton
+                        aria-label='close'
+                        onClick={() => {}}
+                        sx={{ paddingTop: '15px' }}
+                    >
+                        <Close sx={{ color: '#FFFFFF' }} />
+                    </IconButton>
+                </Box>
+                { step === 0 && (
+                    <UploadMedia onChangeFile={handleChangeFile} />
+                )}
+                { step === 1 && (
+                    <PostPreview
+                        url={fileUrl ?? ''} />
+                )}
+            </Box>
+        </Dialog>
+    )
+}
