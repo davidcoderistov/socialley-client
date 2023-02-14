3b9811378daf3bf4ba5b0829459a2108be942467
davidcoderistov <davidcoderistov@gmail.com>
2023-02-14 15:35:00 +0100

    Write getFollowedUsersPostsPaginated client query

---
+++ client/src/graphql/queries/posts/index.ts
@@ -0,0 +1,28 @@
+import { gql } from '@apollo/client'
+
+
+export const GET_FOLLOWED_USERS_POSTS_PAGINATED = gql`
+    query getFollowedUsersPostsPaginated ($offset: Int!, $limit: Int!) {
+        getFollowedUsersPostsPaginated(offset: $offset, limit: $limit) {
+            data {
+                _id
+                title
+                photoURL
+                videoURL
+                user {
+                    _id
+                    username
+                    avatarURL
+                }
+                firstLikeUser {
+                    _id
+                    username
+                }
+                likesCount
+                commentsCount
+                createdAt
+            }
+            total
+        }
+    }
+`
