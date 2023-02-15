0d09b3d94b5b5eb7b38f4b46abf4ead20a1fbe07
davidcoderistov <davidcoderistov@gmail.com>
2023-02-15 16:47:30 +0100

    Create UserLikesModal component

---
+++ client/src/components/UserLikesModal/index.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Box, Dialog, IconButton, Typography } from '@mui/material'
+import UserList from './UserList'
+import { Close } from '@mui/icons-material'
+
+
+interface User {
+    _id: string
+    username: string
+    firstName: string
+    lastName: string
+    avatarURL: string | null
+    following: boolean
+    isFollowingLoading: boolean
+}
+
+interface Props {
+    open: boolean
+    onCloseModal: () => void
+    users: User[]
+    isInitialLoading: boolean
+    isMoreLoading: boolean
+    hasMoreUsers: boolean
+    onFetchMoreUsers: () => void
+    onFollowUser: (userId: string) => void
+    onUnfollowUser: (userId: string) => void
+}
+
+export default function UserLikesModal (props: Props) {
+
+    return (
+        <Dialog
+            open={props.open}
+            fullWidth
+            PaperProps={{
+                sx: {
+                    backgroundColor: '#262626',
+                    borderRadius: '20px',
+                    maxWidth: '400px',
+                    height: '480px'
+                }
+            }}
+        >
+            <Box
+                component='div'
+                display='flex'
+                flexDirection='row'
+                justifyContent='space-between'
+                alignItems='center'
+            >
+                <Box component='div' />
+                <Typography color='#FFFFFF'>Likes</Typography>
+                <IconButton
+                    aria-label='close'
+                    onClick={props.onCloseModal}
+                    sx={{ paddingTop: '15px' }}
+                >
+                    <Close sx={{ color: '#FFFFFF' }} />
+                </IconButton>
+            </Box>
+            <UserList
+                users={props.users}
+                isInitialLoading={props.isInitialLoading}
+                isMoreLoading={props.isMoreLoading}
+                hasMoreUsers={props.hasMoreUsers}
+                onFetchMoreUsers={props.onFetchMoreUsers}
+                onFollowUser={props.onFollowUser}
+                onUnfollowUser={props.onUnfollowUser} />
+        </Dialog>
+    )
+}
