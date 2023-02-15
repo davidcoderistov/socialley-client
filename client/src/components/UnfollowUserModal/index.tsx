d06ce8be2a1225bfdef999ebfd4b1f7286aed1a2
davidcoderistov <davidcoderistov@gmail.com>
2023-02-15 15:05:59 +0100

    Create UnfollowUserModal component

---
+++ client/src/components/UnfollowUserModal/index.tsx
@@ -0,0 +1,103 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Dialog from '@mui/material/Dialog'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import UserAvatar from '../UserAvatar'
+import { Close } from '@mui/icons-material'
+
+
+interface Props {
+    open: boolean
+    user: {
+        _id: string
+        firstName: string
+        lastName: string
+        username: string
+        avatarURL: string | null
+    }
+    onUnfollowUser: (userId: string) => void
+    onCloseModal: () => void
+}
+
+export default function UnfollowUserModal (props: Props) {
+
+    const handleUnfollowUser = () => props.onUnfollowUser(props.user._id)
+
+    return (
+        <Dialog
+            open={props.open}
+            fullWidth
+            PaperProps={{
+                sx: {
+                    backgroundColor: '#262626',
+                    borderRadius: '20px',
+                    maxWidth: '400px',
+                    paddingBottom: '10px',
+                }
+            }}
+        >
+            <Box
+                component='div'
+                display='flex'
+                flexDirection='row'
+                justifyContent='flex-end'
+                alignItems='center'
+            >
+                <IconButton
+                    aria-label='close'
+                    onClick={props.onCloseModal}
+                    sx={{ paddingTop: '15px' }}
+                >
+                    <Close sx={{ color: '#FFFFFF' }} />
+                </IconButton>
+            </Box>
+            <Box
+                component='div'
+                display='flex'
+                flexDirection='column'
+                justifyContent='center'
+                alignItems='center'
+                paddingTop='10px'
+                paddingBottom='30px'
+            >
+                <UserAvatar
+                    firstName={props.user.firstName}
+                    lastName={props.user.lastName}
+                    size={80}
+                    backgroundColor='#616161'
+                />
+            </Box>
+            <Box
+                component='div'
+                display='flex'
+                flexDirection='column'
+                justifyContent='center'
+                alignItems='center'
+                paddingBottom='30px'
+            >
+                <Typography
+                    variant='body2'
+                    sx={{
+                        color: '#FFFFFF',
+                        textAlign: 'center',
+                    }}
+                >
+                    If you change your mind, you'll have to request to follow <b>{ props.user.username }</b> again.
+                </Typography>
+            </Box>
+            <Button
+                variant='text'
+                fullWidth
+                color='error'
+                sx={{
+                    color: '#ED4956',
+                }}
+                onClick={handleUnfollowUser}
+            >
+                Unfollow
+            </Button>
+        </Dialog>
+    )
+}
