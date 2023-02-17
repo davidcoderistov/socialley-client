7b2eb739d58beb30fb7ae3fd1355d2c64f4a3888
davidcoderistov <davidcoderistov@gmail.com>
2023-02-17 12:15:53 +0100

    Add updateFollowedUserPostFavoriteStatus mutation

--- client/src/apollo/mutations/posts/followedUsersPosts.ts
+++ client/src/apollo/mutations/posts/followedUsersPosts.ts
@@ -38,3 +38,41 @@ export function updateFollowedUserPostLikedStatus (options: UpdateFollowedUserPo
         success,
     }
 }
+
+interface UpdateFollowedUserPostFavoriteStatus {
+    followedUsersPosts: FollowedUsersPostsQueryData
+    postId: string
+    favorite: boolean
+}
+
+interface UpdateFollowedUserPostFavoriteStatusReturnValue {
+    followedUsersPosts: FollowedUsersPostsQueryData
+    success: boolean
+}
+
+export function updateFollowedUserPostFavoriteStatus (options: UpdateFollowedUserPostFavoriteStatus): UpdateFollowedUserPostFavoriteStatusReturnValue {
+    const { followedUsersPosts, postId, favorite } = options
+
+    let success = false
+    const followedUsersPostsResult = {
+        ...followedUsersPosts,
+        getFollowedUsersPostsPaginated: {
+            ...followedUsersPosts.getFollowedUsersPostsPaginated,
+            data: followedUsersPosts.getFollowedUsersPostsPaginated.data.map(post => {
+                if (post._id === postId) {
+                    success = true
+                    return {
+                        ...post,
+                        favorite,
+                    }
+                }
+                return post
+            })
+        }
+    }
+
+    return {
+        followedUsersPosts: followedUsersPostsResult,
+        success,
+    }
+}
