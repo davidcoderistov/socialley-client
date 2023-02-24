089dcac86de65858d41402891b078dc22b1a48bb
davidcoderistov <davidcoderistov@gmail.com>
2023-02-24 12:48:17 +0100

    Create initial version of HomePage component

--- client/src/pages/HomePage/index.tsx
+++ client/src/pages/HomePage/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import Box from '@mui/material/Box'
+import FollowedUsersPosts from '../../components/FollowedUsersPosts'
+import TopFiveSuggestedUsers from '../../components/TopFiveSuggestedUsers'
 
 
 export default function HomePage () {
 
     return (
-        <div>
-            Home Page
-        </div>
+        <Box
+            component='div'
+            display='flex'
+            flexDirection='row'
+            width='100%'
+            padding='50px'
+        >
+            <FollowedUsersPosts
+                component='div'
+                display='flex'
+                flexDirection='column'
+                justifyContent='center'
+                alignItems='center'
+                flex='1 1 auto' />
+            <Box
+                component='div'
+                display='flex'
+                flexDirection='column'
+                flex='0 0 440px'
+            >
+                <TopFiveSuggestedUsers />
+            </Box>
+        </Box>
     )
 }
