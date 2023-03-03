6e24899dc070538a49e1e5098cbbcb6e24b4c532
davidcoderistov <davidcoderistov@gmail.com>
2023-03-03 14:39:58 +0100

    Create UserActionButton component

---
+++ client/src/components/UserActionButton/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import LoadingButton from '@mui/lab/LoadingButton'
+
+
+interface UserActionButtonProps {
+    variant: 'primary' | 'secondary'
+    text: string
+    minWidth?: number
+    contained: boolean
+    loading: boolean
+    onClick: () => void
+}
+
+export default function UserActionButton (props: UserActionButtonProps) {
+
+    const primary = props.variant === 'primary'
+
+    return (
+        <LoadingButton
+            variant={props.contained ? 'contained' : 'text'}
+            sx={props.contained ? {
+                textTransform: 'none',
+                borderRadius: '10px',
+                ...!!props.minWidth && { minWidth: props.minWidth },
+                backgroundColor: primary ? '#0095F6': '#EFEFEF',
+                ...!primary && { color: '#000000' },
+                '&:hover': {
+                    backgroundColor: primary ? '#1877F2' : '#DBDBDB'
+                },
+                '&.MuiLoadingButton-loading': {
+                    backgroundColor: primary ? '#0095F6' : '#FFFFFF',
+                },
+                '.MuiLoadingButton-loadingIndicator': {
+                    color: primary ? '#FFFFFF' : '#000000',
+                }
+            } : {
+                textTransform: 'none',
+                color: primary ? '#0095F6' : '#DBDBDB',
+                '&:hover': {
+                    color: '#FFFFFF',
+                },
+                '.MuiLoadingButton-loadingIndicator': {
+                    color: primary ? '#0095F6' : '#DBDBDB',
+                }
+            }}
+            loading={props.loading}
+            onClick={props.onClick}
+        >
+            { props.text }
+        </LoadingButton>
+    )
+}
