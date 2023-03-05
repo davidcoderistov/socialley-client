f3adafe2a1d09a8cdd15228cc028c23388a95f4c
davidcoderistov <davidcoderistov@gmail.com>
2023-03-05 17:43:00 +0100

    Add favorite post for user when marking posts as favorite

--- client/src/hooks/graphql/posts/useMarkPostAsFavorite.ts
+++ client/src/hooks/graphql/posts/useMarkPostAsFavorite.ts
@@ -3,6 +3,8 @@ import { useSnackbar } from 'notistack'
 import { MARK_USER_POST_AS_FAVORITE } from '../../../graphql/mutations/posts'
 import { useUpdatePostDetailsFavoriteLoadingStatus } from './useUpdatePostDetailsFavoriteLoadingStatus'
 import { useUpdatePostDetailsFavoriteStatus } from './useUpdatePostDetailsFavoriteStatus'
+import { useAddFavoritePostForUser } from './useAddFavoritePostForUser'
+import { PostDetails } from '../../../types'
 
 
 export function useMarkPostAsFavorite () {
@@ -13,8 +15,9 @@ export function useMarkPostAsFavorite () {
 
     const updatePostDetailsFavoriteLoadingStatus = useUpdatePostDetailsFavoriteLoadingStatus()
     const updatePostDetailsFavoriteStatus = useUpdatePostDetailsFavoriteStatus()
+    const addFavoritePostForUser = useAddFavoritePostForUser()
 
-    return (postId: string) => {
+    return (postId: string, postDetails: PostDetails) => {
         updatePostDetailsFavoriteLoadingStatus(postId, true)
         markPostAsFavorite({
             variables: {
@@ -22,6 +25,12 @@ export function useMarkPostAsFavorite () {
             }
         }).then(() => {
             updatePostDetailsFavoriteStatus(postId, true)
+            addFavoritePostForUser({
+                _id: postDetails._id,
+                title: postDetails.title,
+                photoURL: postDetails.photoURL,
+                videoURL: postDetails.videoURL,
+            })
         }).catch(() => {
             updatePostDetailsFavoriteLoadingStatus(postId, false)
             enqueueSnackbar(`Could not mark this post as favorite`, { variant: 'error' })
