1e1e1e1dbe1ba75caab52de104393cdac2f4b4cd
davidcoderistov <davidcoderistov@gmail.com>
2023-03-05 15:10:31 +0100

    Implement view post in UserPostsFeed component

--- client/src/components/UserPostsFeed/index.tsx
+++ client/src/components/UserPostsFeed/index.tsx
@@ -1,9 +1,22 @@
-import React, { useEffect, useMemo } from 'react'
-import { useLazyQuery } from '@apollo/client'
-import { GET_POSTS_FOR_USER } from '../../graphql/queries/posts'
-import { GetPostsForUserQueryType } from '../../graphql/types/queries/posts'
+import React, { useState, useEffect, useMemo } from 'react'
+import { useQuery, useLazyQuery } from '@apollo/client'
+import {
+    useComments,
+    useCreateComment,
+    useLikeComment,
+    useUnlikeComment,
+    useLikePost,
+    useUnlikePost,
+    useMarkPostAsFavorite,
+    useUnmarkPostAsFavorite,
+    useUpdateFollowedUserPostCommentsCount,
+} from '../../hooks/graphql/posts'
+import { GET_POSTS_FOR_USER, GET_POST_DETAILS } from '../../graphql/queries/posts'
+import { GetPostsForUserQueryType, GetPostDetailsQueryType } from '../../graphql/types/queries/posts'
 import { BoxProps } from '@mui/material/Box'
 import PostsFeed from '../PostsFeed'
+import PostView from '../PostView/PostView'
+import { PostDetails as PostViewDetails } from '../../types'
 
 
 interface UserPostsFeedProps {
@@ -13,6 +26,13 @@ interface UserPostsFeedProps {
 }
 
 export default function UserPostsFeed ({ boxProps = {}, dense = false, shouldSkipQuery = false }: UserPostsFeedProps) {
+
+    const [viewingPostId, setViewingPostId] = useState<string | null>(null)
+
+    const postDetails = useQuery<GetPostDetailsQueryType>(GET_POST_DETAILS, {
+        variables: { postId: viewingPostId },
+        skip: !viewingPostId,
+    })
 
     const [getUserPosts, userPosts] = useLazyQuery<GetPostsForUserQueryType>(GET_POSTS_FOR_USER)
 
@@ -54,18 +74,102 @@ export default function UserPostsFeed ({ boxProps = {}, dense = false, shouldSki
         })
     }
 
-    const handleClickPost = (postId: string) => {
+    const postViewDetails: PostViewDetails | null = useMemo(() => {
+        if (postDetails.data) {
+            return {
+                ...postDetails.data.getPostDetails,
+                ...postDetails.data.getPostDetails.post,
+                user: {
+                    ...postDetails.data.getPostDetails.user,
+                    following: false,
+                    isFollowingLoading: false,
+                },
+            }
+        }
+        return null
+    }, [postDetails.data])
 
+    const handleClickPost = (postId: string) => {
+        setViewingPostId(postId)
+    }
+
+    const handleCloseViewPost = () => {
+        setViewingPostId(null)
+    }
+
+    const likePost = useLikePost()
+    const unlikePost = useUnlikePost()
+
+    const handleLikePost = (postId: string, liked: boolean) => {
+        if (liked) {
+            likePost(postId)
+        } else {
+            unlikePost(postId, postViewDetails as PostViewDetails)
+        }
+    }
+
+    const markPostAsFavorite = useMarkPostAsFavorite()
+    const unmarkPostAsFavorite = useUnmarkPostAsFavorite()
+
+    const handleBookmarkPost = (postId: string, favorite: boolean) => {
+        if (favorite) {
+            markPostAsFavorite(postId)
+        } else {
+            unmarkPostAsFavorite(postId)
+        }
+    }
+
+    const { commentsForPost, fetchMoreComments, hasMoreComments } = useComments({ postId: viewingPostId })
+
+    const likeComment = useLikeComment()
+    const unlikeComment = useUnlikeComment()
+
+    const handleLikeComment = (commentId: string, postId: string, liked: boolean) => {
+        if (liked) {
+            likeComment(commentId, postId)
+        } else {
+            unlikeComment(commentId, postId)
+        }
+    }
+
+    const { createComment, createCommentData } = useCreateComment({ postId: viewingPostId })
+
+    const updateFollowedUserPostCommentsCount = useUpdateFollowedUserPostCommentsCount()
+
+    const handlePostComment = (comment: string) => {
+        createComment(
+            comment,
+            () => updateFollowedUserPostCommentsCount(viewingPostId as string)
+        )
     }
 
     return (
-        <PostsFeed
-            boxProps={boxProps}
-            dense={dense}
-            posts={posts}
-            isPostsLoading={userPosts.loading}
-            hasMorePosts={hasMorePosts}
-            onFetchMorePosts={handleFetchMorePosts}
-            onClickPost={handleClickPost} />
+        <>
+            <PostsFeed
+                boxProps={boxProps}
+                dense={dense}
+                posts={posts}
+                isPostsLoading={userPosts.loading}
+                hasMorePosts={hasMorePosts}
+                onFetchMorePosts={handleFetchMorePosts}
+                onClickPost={handleClickPost} />
+            { viewingPostId && (
+                <PostView
+                    postDetails={postViewDetails}
+                    isPostDetailsLoading={postDetails.loading}
+                    onClickUser={() => {}}
+                    onFollowUser={() => {}}
+                    onLikePost={handleLikePost}
+                    onBookmarkPost={handleBookmarkPost}
+                    comments={commentsForPost.data?.getCommentsForPost.data ?? []}
+                    hasMoreComments={hasMoreComments}
+                    commentsLoading={commentsForPost.loading}
+                    onFetchMoreComments={fetchMoreComments}
+                    onLikeComment={handleLikeComment}
+                    isCommentPosting={createCommentData.loading}
+                    onPostComment={handlePostComment}
+                    onClose={handleCloseViewPost} />
+            )}
+        </>
     )
 }
