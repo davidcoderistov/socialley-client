b5daeddf626912b584f357367c215ec94491f4d9
davidcoderistov <davidcoderistov@gmail.com>
2023-03-09 15:16:09 +0100

    Complete profile page

--- client/src/pages/ProfilePage/index.tsx
+++ client/src/pages/ProfilePage/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Box from '@mui/material/Box'
+import MyProfile from '../../components/MyProfile'
 
 
 export default function ProfilePage () {
 
     return (
-        <Box color='#FFFFFF'>
-            Profile Page
+        <Box color='#FFFFFF' width='100%' marginTop='40px'>
+            <MyProfile />
         </Box>
     )
 }
