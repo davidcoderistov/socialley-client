8efa15c3e1dc9b16caa9b82760e1a32715b88e35
davidcoderistov <davidcoderistov@gmail.com>
2023-03-14 22:17:26 +0100

    Add addFollowNotification apollo mutation

--- client/src/apollo/mutations/users/followNotificationsForUser.ts
+++ client/src/apollo/mutations/users/followNotificationsForUser.ts
@@ -1,5 +1,7 @@
 import { GetFollowNotificationsForUserQueryType } from '../../../graphql/types/queries/users'
+import { FollowNotification } from '../../../graphql/types/models'
 import { updateOneFollowNotification } from '../../utils'
+import _uniqBy from 'lodash/uniqBy'
 
 
 interface UpdateFollowingLoadingStatusOptions {
@@ -54,7 +56,40 @@ export function updateFollowingStatus (options: UpdateFollowingStatusOptions): U
     })
 }
 
+interface AddFollowNotificationOptions {
+    followNotificationsForUser: GetFollowNotificationsForUserQueryType
+    followNotification: FollowNotification
+}
+
+interface AddFollowNotificationReturnValue {
+    followNotificationsForUser: GetFollowNotificationsForUserQueryType
+}
+
+export function addFollowNotification (options: AddFollowNotificationOptions): AddFollowNotificationReturnValue {
+    const { followNotificationsForUser, followNotification } = options
+
+    const exists = followNotificationsForUser.getFollowNotificationsForUser.data.some(notification =>
+        notification.followableUser.user._id === followNotification.followableUser.user._id)
+
+    return {
+        followNotificationsForUser: {
+            ...followNotificationsForUser,
+            getFollowNotificationsForUser: {
+                ...followNotificationsForUser.getFollowNotificationsForUser,
+                data: _uniqBy([
+                    followNotification,
+                    ...followNotificationsForUser.getFollowNotificationsForUser.data
+                ], followNotification => followNotification.followableUser.user._id),
+                total: exists ?
+                    followNotificationsForUser.getFollowNotificationsForUser.total :
+                    followNotificationsForUser.getFollowNotificationsForUser.total + 1
+            }
+        }
+    }
+}
+
 export default {
     updateFollowingLoadingStatus,
     updateFollowingStatus,
+    addFollowNotification,
 }
