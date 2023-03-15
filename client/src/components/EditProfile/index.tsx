6d60ff04d8b27afeb436897d302a8724d8ba33ad
davidcoderistov <davidcoderistov@gmail.com>
2023-03-15 23:38:38 +0100

    Handle change profile photo

--- client/src/components/EditProfile/index.tsx
+++ client/src/components/EditProfile/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSnackbar } from 'notistack'
 import { useMutation, ApolloError } from '@apollo/client'
-import { EDIT_USER } from '../../graphql/mutations/auth'
-import { EditUserMutationType } from '../../graphql/types/mutations/auth'
+import { CHANGE_PROFILE_PHOTO, EDIT_USER } from '../../graphql/mutations/auth'
+import { ChangeProfilePhotoMutationType, EditUserMutationType } from '../../graphql/types/mutations/auth'
 import { useLoggedInUser } from '../../hooks/misc'
 import { useForm, FieldPath } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
@@ -12,7 +12,15 @@ import CssBaseline from '@mui/material/CssBaseline'
 import TextField from '@mui/material/TextField'
 import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
+import IconButton from '@mui/material/IconButton'
+import UserAvatar from '../UserAvatar'
+import FileUpload from '../FileUpload'
+import LoadingIconButton from '../LoadingIconButton'
+import { Check, Close } from '@mui/icons-material'
+import AvatarEditor from 'react-avatar-editor'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import { getValidationError } from '../../utils'
 
@@ -30,6 +38,10 @@ interface FormData {
     email: string
 }
 
+interface Editor {
+    getImage: () => HTMLCanvasElement
+}
+
 const validationSchema = yup.object().shape({
     firstName: yup.string().required('First name is required'),
     lastName: yup.string().required('Last name is required'),
@@ -41,6 +53,11 @@ export default function EditProfile () {
 
     const [loggedInUser, setLoggedInUser] = useLoggedInUser()
 
+    const [uploadFile, setUploadFile] = useState<File | null>(null)
+    const [editor, setEditor] = useState<Editor | null>(null)
+    const [isSavingProfilePhoto, setIsSavingProfilePhoto] = useState(false)
+
+    const [changeProfilePhoto] = useMutation<ChangeProfilePhotoMutationType>(CHANGE_PROFILE_PHOTO)
     const [editUser, { loading }] = useMutation<EditUserMutationType>(EDIT_USER)
 
     const { enqueueSnackbar } = useSnackbar()
@@ -89,6 +106,67 @@ export default function EditProfile () {
         })
     }
 
+    const handleChangeProfilePhoto = (file: File) => {
+        setUploadFile(file)
+    }
+
+    const handleSaveProfilePhoto = () => {
+        if (editor && uploadFile) {
+            setIsSavingProfilePhoto(true)
+
+            const delegate = new Promise((resolve) => {
+                resolve(undefined)
+            })
+
+            delegate.then(() => {
+                const canvas = editor.getImage().toDataURL()
+                fetch(canvas)
+                    .then(res => res.blob())
+                    .then(blob => {
+                        const photo = new File([blob], uploadFile.name, { type: blob.type })
+                        changeProfilePhoto({
+                            variables: {
+                                photo
+                            },
+                            context: {
+                                hasUpload: true
+                            }
+                        }).then(data => {
+                            const user = data.data?.changeProfilePhoto
+                            if (user) {
+                                setLoggedInUser({
+                                    ...loggedInUser,
+                                    ...user.user,
+                                    accessToken: user.accessToken,
+                                })
+                                enqueueSnackbar('You have changed your profile photo successfully', { variant: 'success' })
+                                setUploadFile(null)
+                                setEditor(null)
+                            } else {
+                                enqueueSnackbar('Profile photo could not be changed. Please try again later', { variant: 'error' })
+                            }
+                        }).catch(() => {
+                            enqueueSnackbar('Profile photo could not be changed. Please try again later', { variant: 'error' })
+                        }).finally(() => setIsSavingProfilePhoto(false))
+                    }).catch(() => {
+                    setIsSavingProfilePhoto(false)
+                    enqueueSnackbar('Profile photo could not be changed. Please try again later', { variant: 'error' })
+                })
+            })
+        } else {
+            enqueueSnackbar('Profile photo could not be changed. Please try again later', { variant: 'error' })
+        }
+    }
+
+
+    const handleDiscardProfilePhoto = () => {
+        setUploadFile(null)
+    }
+
+    const setEditorRef = (editor: Editor | null) => {
+        setEditor(editor)
+    }
+
     return (
         <Container component='main' maxWidth='sm'>
             <CssBaseline />
@@ -100,6 +178,91 @@ export default function EditProfile () {
                     mt: 4,
                 }}
             >
+                <Grid container spacing={2} sx={{ mb: 2 }}>
+                    <Grid item xs={12}>
+                        <Box
+                            component='div'
+                            display='flex'
+                            flexDirection='row'
+                            justifyContent='flex-start'
+                            alignItems='center'
+                            columnGap='10px'
+                        >
+                            <UserAvatar
+                                size={56}
+                                firstName={loggedInUser.firstName}
+                                lastName={loggedInUser.lastName}
+                                photoURL={loggedInUser.avatarURL} />
+                            <Box
+                                component='div'
+                                display='flex'
+                                flexDirection='column'
+                                justifyContent='center'
+                                alignItems='flex-start'
+                            >
+                                <Typography
+                                    variant='body1'
+                                    color='#FFFFFF'
+                                >
+                                    { loggedInUser.username }
+                                </Typography>
+                                <Button
+                                    variant='text'
+                                    color='primary'
+                                    component='label'
+                                    sx={{ textTransform: 'none', padding: 0, '&:hover': { color: '#FFFFFF' }}}
+                                >
+                                    Change profile photo
+                                    <FileUpload
+                                        mimeTypes={['image/jpeg', 'image/png']}
+                                        onChangeFile={handleChangeProfilePhoto} />
+                                </Button>
+                            </Box>
+                        </Box>
+                    </Grid>
+                </Grid>
+                { uploadFile && (
+                    <Grid container spacing={2} sx={{ mb: 2 }}>
+                        <Grid item xs={12} sx={{ '&.MuiGrid-item': { paddingTop: 0 } }}>
+                            <Box
+                                component='div'
+                                display='flex'
+                                flexDirection='column'
+                                justifyContent='center'
+                                alignItems='center'
+                            >
+                                <Box
+                                    component='div'
+                                    display='flex'
+                                    flexDirection='row'
+                                    justifyContent='center'
+                                    alignItems='center'
+                                    columnGap='5px'
+                                >
+                                    <LoadingIconButton
+                                        color='#4caf50'
+                                        loading={isSavingProfilePhoto}
+                                        iconComponent={<Check />}
+                                        onClick={handleSaveProfilePhoto} />
+                                    <IconButton sx={{ padding: 0 }} onClick={handleDiscardProfilePhoto}>
+                                        <Close sx={{ color: '#ED4956' }}/>
+                                    </IconButton>
+                                </Box>
+                                <AvatarEditor
+                                    ref={setEditorRef}
+                                    image={uploadFile}
+                                    width={150}
+                                    height={150}
+                                    border={5}
+                                    borderRadius={120}
+                                    color={[255, 255, 255, 0.4]}
+                                    scale={1.2}
+                                    rotate={0}
+                                />
+                            </Box>
+                        </Grid>
+                    </Grid>
+                )}
                 <Box component='form' onSubmit={handleSubmit(handleEditUser)} noValidate sx={{ mt: 1 }}>
                     <ThemeProvider theme={darkTheme}>
                         <Grid container spacing={2}>
