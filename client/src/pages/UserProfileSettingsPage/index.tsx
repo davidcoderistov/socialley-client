3e281034ec19e190ded2c6b2588a8e7ea9dd5707
davidcoderistov <davidcoderistov@gmail.com>
2023-03-18 20:24:25 +0100

    Create UserProfileSettingsPage component

---
+++ client/src/pages/UserProfileSettingsPage/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import UserProfileSettings from '../../components/UserProfileSettings'
+
+
+export default function UserProfileSettingsPage () {
+
+    return (
+        <Box
+            component='div'
+            display='block'
+            width='100%'
+        >
+            <UserProfileSettings />
+        </Box>
+    )
+}
