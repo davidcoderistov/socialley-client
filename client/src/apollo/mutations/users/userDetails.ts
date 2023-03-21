4d05b14f2d13f5ab434e03d1930323c6013d909c
davidcoderistov <davidcoderistov@gmail.com>
2023-03-21 22:13:01 +0100

    Add apollo mutations for getUserDetails graphql query

--- client/src/apollo/mutations/users/userDetails.ts
+++ client/src/apollo/mutations/users/userDetails.ts
@@ -1,4 +1,5 @@
 import { GetUserDetailsQueryType } from '../../../graphql/types/queries/users'
+import { User } from '../../../graphql/types/models'
 
 
 interface UpdateFollowingLoadingStatusOptions {
@@ -39,7 +40,79 @@ export function updateFollowingStatus (options: UpdateFollowingStatusOptions): G
     }
 }
 
+interface IncrementFollowingCountOptions {
+    userDetails: GetUserDetailsQueryType
+}
+
+export function incrementFollowingCount (options: IncrementFollowingCountOptions): GetUserDetailsQueryType {
+    const { userDetails } = options
+
+    return {
+        ...userDetails,
+        getUserDetails: {
+            ...userDetails.getUserDetails,
+            followingCount: userDetails.getUserDetails.followingCount + 1,
+        }
+    }
+}
+
+interface DecrementFollowingCountOptions {
+    userDetails: GetUserDetailsQueryType
+}
+
+export function decrementFollowingCount (options: DecrementFollowingCountOptions): GetUserDetailsQueryType {
+    const { userDetails } = options
+
+    return {
+        ...userDetails,
+        getUserDetails: {
+            ...userDetails.getUserDetails,
+            followingCount: userDetails.getUserDetails.followingCount - 1,
+        }
+    }
+}
+
+interface IncrementFollowersCountOptions {
+    userDetails: GetUserDetailsQueryType
+    latestFollower: Pick<User, '_id' | 'username'> | null
+}
+
+export function incrementFollowersCount (options: IncrementFollowersCountOptions): GetUserDetailsQueryType {
+    const { userDetails, latestFollower } = options
+
+    return {
+        ...userDetails,
+        getUserDetails: {
+            ...userDetails.getUserDetails,
+            followersCount: userDetails.getUserDetails.followersCount + 1,
+            latestFollower,
+        }
+    }
+}
+
+interface DecrementFollowersCountOptions {
+    userDetails: GetUserDetailsQueryType
+    latestFollower: Pick<User, '_id' | 'username'> | null
+}
+
+export function decrementFollowersCount (options: DecrementFollowersCountOptions): GetUserDetailsQueryType {
+    const { userDetails, latestFollower } = options
+
+    return {
+        ...userDetails,
+        getUserDetails: {
+            ...userDetails.getUserDetails,
+            followersCount: userDetails.getUserDetails.followersCount - 1,
+            latestFollower,
+        }
+    }
+}
+
 export default {
     updateFollowingLoadingStatus,
     updateFollowingStatus,
+    incrementFollowingCount,
+    decrementFollowingCount,
+    incrementFollowersCount,
+    decrementFollowersCount,
 }
