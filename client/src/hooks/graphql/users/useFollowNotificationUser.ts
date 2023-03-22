2d31bf944949ea9703ccb1bbbda4f0653c7b5222
davidcoderistov <davidcoderistov@gmail.com>
2023-03-22 15:09:12 +0100

    Use follow hook in useFollowNotificationUser hook

--- client/src/hooks/graphql/users/useFollowNotificationUser.ts
+++ client/src/hooks/graphql/users/useFollowNotificationUser.ts
@@ -1,40 +1,26 @@
-import { useMutation } from '@apollo/client'
-import { useFollowUpdateUserConnections } from './useFollowUpdateUserConnections'
-import { FOLLOW_USER } from '../../../graphql/mutations/users'
-import { FollowUserMutationType } from '../../../graphql/types/mutations/users'
-import { useSnackbar } from 'notistack'
+import { useFollowUser } from './useFollowUser'
 import { useUpdateNotificationUserFollowingLoadingStatus } from './useUpdateNotificationUserFollowingLoadingStatus'
 import { useUpdateNotificationUserFollowingStatus } from './useUpdateNotificationUserFollowingStatus'
 
 
 export function useFollowNotificationUser () {
 
-    const { enqueueSnackbar } = useSnackbar()
-
-    const [followUser] = useMutation<FollowUserMutationType>(FOLLOW_USER)
-    const updateFollowUserConnections = useFollowUpdateUserConnections()
+    const followUser = useFollowUser()
 
     const updateFollowingLoadingStatus = useUpdateNotificationUserFollowingLoadingStatus()
     const updateFollowingStatus = useUpdateNotificationUserFollowingStatus()
 
     return (userId: string) => {
-        updateFollowingLoadingStatus(userId, true)
-        followUser({
-            variables: {
-                followedUserId: userId
+        followUser(userId, {
+            onStart () {
+                updateFollowingLoadingStatus(userId, true)
+            },
+            onSuccess () {
+                updateFollowingStatus(userId, true)
+            },
+            onError () {
+                updateFollowingLoadingStatus(userId, false)
             }
-        }).then(follow => {
-            updateFollowingStatus(userId, true)
-            const followedUser = follow.data?.followUser
-            if (followedUser) {
-                updateFollowUserConnections({
-                    followableUser: followedUser,
-                    isFollowingLoading: false,
-                })
-            }
-        }).catch(() => {
-            updateFollowingLoadingStatus(userId, false)
-            enqueueSnackbar('Could not follow user', { variant: 'error' })
         })
     }
 }
