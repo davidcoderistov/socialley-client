894c3e62049965eb9d2062629741a20350cb1afa
davidcoderistov <davidcoderistov@gmail.com>
2023-03-22 15:38:11 +0100

    Create useUnfollowUpdateUserConnections hook

---
+++ client/src/hooks/graphql/users/useUnfollowUpdateUserConnections.ts
@@ -0,0 +1,82 @@
+import { useApolloClient } from '@apollo/client'
+import { useLoggedInUser } from '../../misc'
+import {
+    GET_USER_DETAILS,
+    GET_FOLLOWING_FOR_USER,
+    GET_FOLLOWERS_FOR_USER,
+} from '../../../graphql/queries/users'
+import {
+    GetUserDetailsQueryType,
+    GetFollowingForUserQueryType,
+    GetFollowersForUserQueryType,
+} from '../../../graphql/types/queries/users'
+import userDetailsMutations from '../../../apollo/mutations/users/userDetails'
+import followingForUserMutations from '../../../apollo/mutations/users/followingForUser'
+import followersForUserMutations from '../../../apollo/mutations/users/followersForUser'
+
+
+export function useUnfollowUpdateUserConnections () {
+
+    const client = useApolloClient()
+
+    const [loggedInUser] = useLoggedInUser()
+
+    return (userId: string) => {
+
+        client.cache.updateQuery({
+            query: GET_USER_DETAILS,
+            variables: { userId: loggedInUser._id },
+        }, (userDetails: GetUserDetailsQueryType | null) => {
+            if (userDetails) {
+                return userDetailsMutations.decrementFollowingCount({
+                    userDetails
+                })
+            }
+        })
+
+        client.cache.updateQuery({
+            query: GET_FOLLOWING_FOR_USER,
+            variables: { userId: loggedInUser._id },
+        }, (followingUsers: GetFollowingForUserQueryType | null) => {
+            if (followingUsers) {
+                const updateQuery = followingForUserMutations.removeFollowingUser({
+                    followingUsers,
+                    userId,
+                })
+                if (!updateQuery.success) {
+                    client.cache.evict({ fieldName: 'getFollowingForUser', args: { userId: loggedInUser._id }})
+                } else {
+                    return updateQuery.followingUsers
+                }
+            }
+        })
+
+        client.cache.updateQuery({
+            query: GET_USER_DETAILS,
+            variables: { userId },
+        }, (userDetails: GetUserDetailsQueryType | null) => {
+            if (userDetails) {
+                return userDetailsMutations.decrementFollowersCount({
+                    userDetails,
+                })
+            }
+        })
+
+        client.cache.updateQuery({
+            query: GET_FOLLOWERS_FOR_USER,
+            variables: { userId },
+        }, (followerUsers: GetFollowersForUserQueryType | null) => {
+            if (followerUsers) {
+                const updateQuery = followersForUserMutations.removeFollowerUser({
+                    followerUsers,
+                    userId: loggedInUser._id,
+                })
+                if (!updateQuery.success) {
+                    client.cache.evict({ fieldName: 'getFollowersForUser', args: { userId }})
+                } else {
+                    return updateQuery.followerUsers
+                }
+            }
+        })
+    }
+}
