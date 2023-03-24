21350d5138a25d3738983a3e78003bc123fcaabe
davidcoderistov <davidcoderistov@gmail.com>
2023-03-24 14:48:02 +0100

    Always scroll to top of the suggested posts page

--- client/src/pages/SuggestedPostsPage/index.tsx
+++ client/src/pages/SuggestedPostsPage/index.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import Box from '@mui/material/Box'
 import SuggestedPostsFeed from '../../components/SuggestedPostsFeed'
 
 
 export default function SuggestedPostsPage () {
 
+    const scrollRef = useRef<HTMLElement>();
+
+    useEffect(() => {
+        if (scrollRef.current) {
+            scrollRef.current.scrollIntoView()
+        }
+    }, [])
+
     return (
         <Box
+            ref={scrollRef}
             component='div'
             width='100%'
             marginTop='30px'
